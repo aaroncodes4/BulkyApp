7ea7c75eed761f30f3f44d2889dd3b7d6a7ea6c4
AaronCodes4 <aaronapraku026@gmail.com>
2024-11-18 21:39:39 +0000

    18th Nov. push

---
+++ Bulk.Utility/UnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public ICategoryRepository CategoryRepository { get; private set; }
+
+        public void save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
